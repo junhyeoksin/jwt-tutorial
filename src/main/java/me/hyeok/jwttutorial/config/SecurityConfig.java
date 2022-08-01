b279d74751ffa4e388c1113fc8a7ff57934ac02e
junhyeoksin <h-kun@naver.com>
2022-08-02 00:16:00 +0900

    Update: SecurityConfig

--- src/main/java/me/hyeok/jwttutorial/config/SecurityConfig.java
+++ src/main/java/me/hyeok/jwttutorial/config/SecurityConfig.java
@@ -8,13 +8,11 @@ import me.hyeok.jwttutorial.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
